257d4aa97fdb09ed87ae87ef41f57bfc6f80d03c
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-09-29 22:10:55 +0500

    Add files via upload

---
+++ 22-9-24.js
@@ -0,0 +1,237 @@
+// let myArray = [ {name : 'awais' ,class : "bscs"} ,
+//                 {name : 'ahmed' ,class : "BBA"}  ,
+//                 {name : 'dawud' ,class : "BSSE"}
+//               ]
+
+// let requiredStudent;
+// console.log("BEFORE : " , requiredStudent);
+
+// myArray.map((item)=>{      if (item.name === "ahmed") {  requiredStudent = item.name      }})
+//     console.log("AFTER : " , requiredStudent);
+
+
+
+//  9- 26-24
+
+// for ( let i = 1 ; i <= 10 ; i++){
+
+// console.log("5 * ",i , " = " , i*5 );
+
+// }
+
+
+// for (let i = 1; i <= 3; i++) {
+
+//     console.log("i value", i);
+
+//     for (let j = 1; j <= 3; j++) {
+
+//         j != i && console.log("j value", j);
+
+
+//     }
+
+
+
+// }
+
+// let  value = ''
+
+// value && console.log("not null");
+// let user  = '';
+
+// (user )&& console.log("not user");
+
+
+// console.log("sfs");
+
+
+// let array = [14 , 15 , 'undefined' , null , 13]
+// console.log(array);
+
+// array.push('abdullah')
+// console.log(array);
+
+
+
+
+// array.pop()
+
+
+// console.log(array.length);
+
+// if (array[4]== 13) {
+//     array.pop()
+    
+// }
+
+// console.log(array);
+
+// function sum(a,b){
+
+//     if (a+b > 10) {
+//         console.log("Outdoor meeting");
+//         return true
+        
+        
+//     }
+    
+//         console.log("Office meeting");
+//         return false
+
+    
+    
+
+// }
+
+//  let isMeetingOutDoor =   sum( 5 ,2)
+//  console.log(isMeetingOutDoor);
+
+
+
+//  let height = 6.99
+//  console.log(Math.ceil(5.1));
+
+
+//  console.log(Math.floor(5.9));
+ 
+ 
+
+// let st = ["Ali" , "umer" , "dnl" , 'fzn']
+// st.push('hmd')
+// st[0] = 'shafiq'
+// st[6] = "asghr"
+// console.log(st);
+
+// //string concatenatio
+// let firstname = "awais"
+// let secondname = "rafiq"
+
+// let fullname = firstname  +  " " + secondname + " is my full name."
+// console.log(fullname);
+
+
+
+// let person = 23;
+// let batch= "23"
+
+// if (batch == person) {
+//     console.log('unified');
+    
+    
+// }
+// else {
+//     console.log('NOT unified');
+
+// }
+
+
+
+
+// let person = 23;
+// let batch= "23"
+
+// if (batch === person || false) {
+//     console.log('unified');
+    
+    
+// }
+// else {
+//     console.log('NOT unified');
+
+// }
+
+
+// let st = [1 , 2 , 9]
+// let rules = 10
+// let diviser = 5
+// let  num1 = 34
+// let num2 = 5
+// let condition = true
+// st = [3 ,5 ,9]
+// diviser = 10
+// rules = 15
+
+// if (     ((false || true)  && (num1 === num2 || true) && ( !condition == false) ) && (st[0] == 3)   ) {
+//     console.log('unified');
+    
+    
+// }
+// else {
+//     console.log('NOT unified');
+
+// }
+
+
+
+
+
+
+
+// let st = [1 , 2 , 9]
+// let rules = 10
+// let diviser = 5
+// let  num1 = 34
+// let num2 = 5
+// let condition = true
+// st = [3 ,5 ,9]
+// diviser = 10
+// rules = 15
+
+// let xo = null
+
+// if (!(xo != null && xo != undefined && xo != '' && xo != false ))
+// if (xo)
+    
+    
+// {
+//     console.log("output");
+    
+// }
+
+
+//  true && true && !false && console.log("hey");
+
+// if (99 && 2) {
+//     console.log("trurrrrrrrrr");
+        
+// }
+
+
+// let x = 23
+
+// if (x >=23) {
+//     console.log("get the food");
+    
+    
+// }
+// else if (x == 23){
+//     console.log(" dont get the food");
+// }
+// else {
+//     console.log(" dont get the food");
+// }
+// console.log(" dont get the food");
+
+let x
+
+
+x= 23
+if (x >= 23) {
+    console.log("Thug 1");
+    
+}  
+else if (true) {
+    console.log("Thug 2");
+
+}
+
+x = 23
+
+if (x === 23) {
+    console.log("Thug 1");
+    
+} else {
+    console.log("Thug 2");
+    
+}
